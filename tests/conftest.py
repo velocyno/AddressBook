abe05e7a6152b45796b66e2e275eab2a606512cc
amarmulyak <a.marmuliak@gmail.com>
2020-07-13 23:46:28 +0300

    Used webdriverdownloader

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
+from webdriverdownloader import ChromeDriverDownloader
 
 
 @pytest.fixture(scope="session")
 def browser_fixture():
+    chrome_driver = ChromeDriverDownloader()
+    chrome_driver.download_and_install()
     driver = webdriver.Chrome(
-        executable_path="C:\\Users\\Andrii\\repositories\\"
-                        "AddressBook\\chromedriver.exe")
+        executable_path="C:\\Users\\Andrii\\bin\\chromedriver.exe")
     yield driver
     driver.quit()

0ce9ccfb7f11b404513ae1b0b64ff8a59e1ed351
amarmulyak <amarm@softserveinc.com>
2020-07-15 15:07:56 +0300

    Fixture+json

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,26 @@ def browser_fixture():
         executable_path="C:\\Users\\Andrii\\bin\\chromedriver.exe")
     yield driver
     driver.quit()
+
+@pytest.fixture
+def data_fixture():
+    dict_data = {
+        'First name:': 'Andrii',
+        'Last name:': 'AQA',
+        'Street Address:': 'Street',
+        'Secondary Address:': 'Street2',
+        'City:': 'Lviv',
+        'State:': 'AK',
+        'Zip code:': '79000',
+        'Country:': 'us',
+        'Birthday:': '6/11/1985',
+        'Color:': "(0, 255, 51)",
+        'Age:': '35',
+        'Website:': 'https://www.site.com',
+        'Phone:': '123456',
+        'Climbing?': 'Yes',
+        'Dancing?': 'Yes',
+        'Reading?': 'Yes',
+        'Note:': 'Test note'
+    }
+    return dict_data
